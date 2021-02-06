e10b3b13910b67c477021937aa3e472f907ad37c
Macsoo <45904652+Macsoo@users.noreply.github.com>
2021-02-06 15:57:58 +0100

    Újradolgozott labirintus generálás, újabb Framework verzió

--- Labirintus/Jatekter.xaml.cs
+++ Labirintus/Jatekter.xaml.cs
@@ -1,16 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using System.IO;
 
 namespace Labirintus
 {
@@ -19,23 +15,26 @@ namespace Labirintus
     /// </summary>
     public partial class Jatekter : Window
     {
+        private Labyrinth lab;
+
         public Jatekter()
         {
             InitializeComponent();
-            Labyrinth lab = new Labyrinth(labirintus);
+            lab = new Labyrinth(labirintus);
             lab.Megjelenit();
         }
+
         public Jatekter(string fajlnev)
         {
             InitializeComponent();
-            Labyrinth lab = new Labyrinth(labirintus, fajlnev);
+            lab = new Labyrinth(labirintus, fajlnev);
             lab.Megjelenit();
         }
 
         private void BtnVissza_Click(object sender, RoutedEventArgs e)
         {
             MessageBoxResult result = MessageBox.Show("Biztosan ki akar lépni a főmenübe?", "Visszalépés", MessageBoxButton.YesNo);
-            if (result==MessageBoxResult.Yes)
+            if (result == MessageBoxResult.Yes)
             {
                 MainWindow mainWindow = new MainWindow();
                 mainWindow.Show();
@@ -45,34 +44,47 @@ namespace Labirintus
 
         private void BtnBalra_Click(object sender, RoutedEventArgs e)
         {
-
         }
 
         private void BtnElore_Click(object sender, RoutedEventArgs e)
         {
-
         }
 
         private void BtnJobbra_Click(object sender, RoutedEventArgs e)
         {
-
         }
 
         private void BtnAutomata_Click(object sender, RoutedEventArgs e)
         {
-
+            lab.GenerateLabyrinth();
         }
     }
-    class Labyrinth
+
+    internal class Labyrinth
     {
         private char[,] Labirint;
         private Grid labirintus;
         private Border[,] cellak;
         private Pos jatekos = new Pos(1, 1);
+
         public Labyrinth(Grid labirintus, string fajlnev)
-        { 
+        {
             this.labirintus = labirintus;
             Labirint = new char[9, 9];
+            for (int i = 0; i < 9; i++)
+            {
+                labirintus.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(1, GridUnitType.Star) });
+                labirintus.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
+            }
+            cellak = new Border[9, 9];
+            for (int i = 0; i < 9 * 9; i++)
+            {
+                cellak[i / 9, i % 9] = new Border();
+                cellak[i / 9, i % 9].KeyDown += Lenyom;
+                labirintus.Children.Add(cellak[i / 9, i % 9]);
+                Grid.SetRow(cellak[i / 9, i % 9], i / 9);
+                Grid.SetColumn(cellak[i / 9, i % 9], i % 9);
+            }
             string[] forras = new string[9];
             int index = 0;
             StreamReader sr = new StreamReader(fajlnev);
@@ -90,25 +102,35 @@ namespace Labirintus
                 }
             }
         }
+
         public Labyrinth(Grid labirintus)
         {
             this.labirintus = labirintus;
-            GenerateLabyrinth();
-        }
-        public void Megjelenit()
-        {
+            Labirint = new char[9, 9];
             for (int i = 0; i < 9; i++)
             {
                 labirintus.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(1, GridUnitType.Star) });
                 labirintus.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
             }
             cellak = new Border[9, 9];
+            for (int i = 0; i < 9 * 9; i++)
+            {
+                cellak[i / 9, i % 9] = new Border();
+                cellak[i / 9, i % 9].KeyDown += Lenyom;
+                labirintus.Children.Add(cellak[i / 9, i % 9]);
+                Grid.SetRow(cellak[i / 9, i % 9], i / 9);
+                Grid.SetColumn(cellak[i / 9, i % 9], i % 9);
+            }
+            //GenerateLabyrinth();
+        }
+
+        public void Megjelenit()
+        {
             for (int sor = 0; sor < 9; sor++)
             {
                 for (int oszlop = 0; oszlop < 9; oszlop++)
                 {
-                    Border cella = new Border();
-                    cella.KeyDown += Lenyom;
+                    Border cella = cellak[sor, oszlop];
                     if (oszlop == 1 && sor == 1)
                     {
                         // 1. sor, 1. oszlop, a játékos alphelyzete
@@ -118,146 +140,105 @@ namespace Labirintus
                         PointCollection myPointCollection = new PointCollection();
                         myPointCollection.Add(Point1);
                         myPointCollection.Add(Point2);
-                        myPointCollection.Add(Point3);                        
-                        cella.Child = new Polygon() { Stroke=Brushes.Black, StrokeThickness=4, Fill=Brushes.Green, Points=myPointCollection, VerticalAlignment = VerticalAlignment.Center, HorizontalAlignment = HorizontalAlignment.Center };
-                        
+                        myPointCollection.Add(Point3);
+                        cella.Child = new Polygon() { Stroke = Brushes.Black, StrokeThickness = 4, Fill = Brushes.Green, Points = myPointCollection, VerticalAlignment = VerticalAlignment.Center, HorizontalAlignment = HorizontalAlignment.Center };
                     }
                     else if (Labirint[sor, oszlop] == '*')
                     {
                         cella.Background = Brushes.Black;
                     }
-                    cellak[sor, oszlop] = cella;
-                    labirintus.Children.Add(cella);
-                    Grid.SetRow(cella, sor);
-                    Grid.SetColumn(cella, oszlop);
+                    else if (Labirint[sor, oszlop] == '.')
+                    {
+                        cella.Background = Brushes.White;
+                    }
                 }
             }
-            void Lenyom(object sender, KeyEventArgs args)
-            {
-
-            }
         }
-        struct Pos
+
+        private void Lenyom(object sender, KeyEventArgs args)
+        {
+        }
+
+        private struct Pos
         {
             public int x, y;
+
             public Pos(int x, int y)
             {
                 this.x = x;
                 this.y = y;
-                
             }
         }
-        struct Jatekos
+
+        private struct Jatekos
         {
             public Pos pozició;
             public char irány;
+
             public Jatekos(Pos poz, char ir)
             {
                 pozició = poz;
                 irány = ir;
             }
+
             public Jatekos(int x, int y, char ir)
             {
                 pozició = new Pos(x, y);
                 irány = ir;
             }
         }
-        enum LabyrinthOptions
+
+        private enum LabyrinthOptions
         {
             Nothing, Road, Wall
         }
-        LabyrinthOptions[] generatedLabyrinth = new LabyrinthOptions[9 * 9];
-        Random rnd = new Random();
-        int canCreateExit = 10;
-        public void GenerateLabyrinth()
-        {
-            Stack<Pos> createdWalls = new Stack<Pos>();
-            createdWalls.Push(new Pos(1, 1));
-            bool CreateWallIfHasTo(Pos position)
-            {
-                if (position.x == 0 || position.y == 0 || position.x == 8 || position.y == 8)
-                {
-                    if (canCreateExit < 1)
-                    {
-                        FillOutline();
-                        generatedLabyrinth[GetIndexAt(position)] = LabyrinthOptions.Road;
-                    }
-                    return true;
-                }
-                else if(generatedLabyrinth[GetIndexAt(position)] == LabyrinthOptions.Nothing)
-                {
-                    int roadCountAroundPosition = 0;
-                    if (generatedLabyrinth[GetIndexAt(new Pos(position.x + 1, position.y))] == LabyrinthOptions.Road) roadCountAroundPosition++;
-                    if (generatedLabyrinth[GetIndexAt(new Pos(position.x - 1, position.y))] == LabyrinthOptions.Road) roadCountAroundPosition++;
-                    if (generatedLabyrinth[GetIndexAt(new Pos(position.x, position.y + 1))] == LabyrinthOptions.Road) roadCountAroundPosition++;
-                    if (generatedLabyrinth[GetIndexAt(new Pos(position.x, position.y - 1))] == LabyrinthOptions.Road) roadCountAroundPosition++;
-                    if (roadCountAroundPosition > 1)
-                    {
-                        generatedLabyrinth[GetIndexAt(position)] = LabyrinthOptions.Wall;
-                        return true;
-                    }
-                    return false;
-                }
-                else
-                {
-                    return true;
-                }
 
-            }
-            int GetIndexAt(Pos position)
-            {
-                return position.y * 9 + position.x;
-            }
-            void FillOutline()
-            {
-                for (int i = 0; i < 9; i++)
-                {
-                    generatedLabyrinth[i] = LabyrinthOptions.Wall;
-                    generatedLabyrinth[71 + i] = LabyrinthOptions.Wall;
-                    generatedLabyrinth[i * 9] = LabyrinthOptions.Wall;
-                    generatedLabyrinth[i * 9 + 8] = LabyrinthOptions.Wall;
-                }
-            }
-            while (generatedLabyrinth.Any(x => x == 0))
-            {
-                Pos currentPos = createdWalls.Peek();
-                generatedLabyrinth[GetIndexAt(currentPos)] = LabyrinthOptions.Road;
-                List<Pos> canGo = new List<Pos>();
-                if (!CreateWallIfHasTo(new Pos(currentPos.x + 1, currentPos.y))) canGo.Add(new Pos(currentPos.x + 1, currentPos.y));
-                if (!CreateWallIfHasTo(new Pos(currentPos.x - 1, currentPos.y))) canGo.Add(new Pos(currentPos.x - 1, currentPos.y));
-                if (!CreateWallIfHasTo(new Pos(currentPos.x, currentPos.y + 1))) canGo.Add(new Pos(currentPos.x, currentPos.y + 1));
-                if (!CreateWallIfHasTo(new Pos(currentPos.x, currentPos.y - 1))) canGo.Add(new Pos(currentPos.x, currentPos.y - 1));
-                if (canGo.Count > 0)
-                {
-                    Pos goTo = canGo[rnd.Next(0, canGo.Count)];
-                    createdWalls.Push(goTo);
-                    canCreateExit--;
-                }
-                else
-                {
-                    createdWalls.Pop();
-                }
-            }
-            Labirint = new char[9, 9];
+        private Random rnd = new Random();
+        private int canCreateExit = 10;
+
+        public async void GenerateLabyrinth()
+        {
+            Stack<Pos> createdRoads = new Stack<Pos>();
+            createdRoads.Push(new Pos(1, 1));
+            bool[,] visited = new bool[9, 9];
+            visited[1, 1] = true;
             for (int y = 0; y < 9; y++)
             {
                 for (int x = 0; x < 9; x++)
                 {
-                    switch(generatedLabyrinth[y * 9 + x])
-                    {
-                        case LabyrinthOptions.Road:
-                            Labirint[x, y] = '.';
-                            break;
-                        case LabyrinthOptions.Wall:
-                            Labirint[x, y] = '*';
-                            break;
-                        default:
-                            throw new Exception("Nem lett tökéletes a labirintus leenerálása!");
-                            //break;
-                    }
+                    Labirint[x, y] = (x % 2 == 1 && y % 2 == 1) ? '.' : '*';
                 }
             }
+            void Visit(Pos position)
+            {
+                List<Pos> unvisitedNeighbours = new List<Pos>();
+                if (IsNotVisited(position.x + 2, position.y)) unvisitedNeighbours.Add(new Pos(position.x + 2, position.y));
+                if (IsNotVisited(position.x - 2, position.y)) unvisitedNeighbours.Add(new Pos(position.x - 2, position.y));
+                if (IsNotVisited(position.x, position.y + 2)) unvisitedNeighbours.Add(new Pos(position.x, position.y + 2));
+                if (IsNotVisited(position.x, position.y - 2)) unvisitedNeighbours.Add(new Pos(position.x, position.y - 2));
+                if (unvisitedNeighbours.Count > 0)
+                {
+                    createdRoads.Push(position);
+                    Pos choosenPos = unvisitedNeighbours[rnd.Next(0, unvisitedNeighbours.Count)];
+                    Labirint[(choosenPos.x + position.x) / 2, (choosenPos.y + position.y) / 2] = '.';
+                    visited[choosenPos.x, choosenPos.y] = true;
+                    createdRoads.Push(choosenPos);
+                }
+            }
+            bool IsNotVisited(int x, int y)
+            {
+                return x > 0 && x < 8 && y > 0 && y < 8 && !visited[x, y];
+            }
+            while (createdRoads.Count > 0)
+            {
+                Pos currentPos = createdRoads.Pop();
+                Visit(currentPos);
+                Megjelenit();
+                await Task.Delay(100);
+            }
+            Labirint[8, 7] = '.';
         }
+
         //int poz = 0;
         /*public void Fordulas(char f, int s, int o)
         {
@@ -271,36 +252,45 @@ namespace Labirintus
 
             Labirint[sor, oszlop] = jatekos;
         }*/
+
         public void Elore(char f, int s, int o)
         {
             char fordul = f; int sor = s; int oszlop = o;
 
-            switch(f)
+            switch (f)
             {
                 case 'V':
-                        if (Labirint[sor + 1, oszlop] != '*' )
-                        { ((Label)cellak[sor + 1, oszlop].Child).Content = 'V';
-                          ((Label)cellak[sor, oszlop].Child).Content = '.';
-                        }
-                        break;
+                    if (Labirint[sor + 1, oszlop] != '*')
+                    {
+                        ((Label)cellak[sor + 1, oszlop].Child).Content = 'V';
+                        ((Label)cellak[sor, oszlop].Child).Content = '.';
+                    }
+                    break;
+
                 case '<':
-                        if (Labirint[sor, oszlop - 1] != '*' ) 
-                        { ((Label)cellak[sor, oszlop - 1].Child).Content = '<';
-                          ((Label)cellak[sor, oszlop].Child).Content = '.';
-                        }
-                        break;
+                    if (Labirint[sor, oszlop - 1] != '*')
+                    {
+                        ((Label)cellak[sor, oszlop - 1].Child).Content = '<';
+                        ((Label)cellak[sor, oszlop].Child).Content = '.';
+                    }
+                    break;
+
                 case '^':
-                        if (Labirint[sor - 1, oszlop] != '*' ) 
-                        { ((Label)cellak[sor - 1, oszlop].Child).Content = '^';
-                          ((Label)cellak[sor, oszlop].Child).Content = '.';
-                        }
-                        break;
+                    if (Labirint[sor - 1, oszlop] != '*')
+                    {
+                        ((Label)cellak[sor - 1, oszlop].Child).Content = '^';
+                        ((Label)cellak[sor, oszlop].Child).Content = '.';
+                    }
+                    break;
+
                 case '>':
-                        if (Labirint[sor, oszlop + 1] != '*' ) 
-                        { ((Label)cellak[sor, oszlop + 1].Child).Content = '>';
-                          ((Label)cellak[sor, oszlop].Child).Content = '.';
-                        }
-                        break;
+                    if (Labirint[sor, oszlop + 1] != '*')
+                    {
+                        ((Label)cellak[sor, oszlop + 1].Child).Content = '>';
+                        ((Label)cellak[sor, oszlop].Child).Content = '.';
+                    }
+                    break;
+
                 default:
                     break;
             }
